6c56e740d2d94b22eca99dda3b877b4c202e3802
Pham Duc <phamduc0701@gmail.com>
2019-12-09 17:33:41 +0700

    Initialize UI

---
+++ AdvancedOpenGL/include/Widget.cpp
@@ -0,0 +1,18 @@
+#include "Widget.h"
+
+//unsigned int Widget::VAO, Widget::VBO;
+//Shader Widget::shader("shader/button/vertex.vs","shader/button/fragment.fs");
+//Shader Widget::shader;
+
+//void Widget::draw(Shader &shader,glm::vec3 &color,unsigned int VAO, int x, int y, int w, int h)
+//{
+//	shader.use();
+//	shader.setVec2("translate", glm::vec2(x, y));
+//	shader.setVec2("scale", glm::vec2(w, h));
+//	shader.setVec3("color", color);
+//
+//	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+//	glBindVertexArray(VAO);
+//	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+//	glBindVertexArray(0);
+//}
---
+++ AdvancedOpenGL/include/Widget.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Shader.h"
+
+class Widget
+{
+public:
+	int x_m, y_m, w_m, h_m;
+	glm::vec3 color_m;
+	Widget* parent = nullptr;
+	std::vector<Widget*> children;
+	Widget(int x = 0, int y = 0, int w = 0, int h = 0,const glm::vec3 &color=glm::vec3(0.5f), Widget* pa = nullptr) :x_m(x), y_m(y), w_m(w), h_m(h),color_m(color), parent(pa) 
+	{}
+
+	virtual void onDragCallback(GLFWwindow*, double, double, double, double) {}
+	virtual void onClick() {}
+	virtual void onDropCallback() {}
+	void draw(Shader& shader, unsigned int VAO)
+	{
+		shader.use();
+		shader.setVec3("color", color_m);
+		shader.setVec2("translate", glm::vec2(x_m, y_m));
+		shader.setVec2("scale", glm::vec2(w_m, h_m));
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindVertexArray(0);
+	}
+
+	
+
+	void drawAll(Shader& shader, unsigned int VAO)
+	{
+		//Draw Parent
+		draw(shader,VAO);
+	
+		//Draw Children
+		for (auto& child : children)
+		{
+			shader.use();
+			child->drawAll(shader,VAO);
+		}
+	}
+
+};
